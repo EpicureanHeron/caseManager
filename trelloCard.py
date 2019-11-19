d877e7f39902d109d885e5884635596e749094ef
epicureanheron <joethor1989@gmail.com>
2019-11-19 11:53:58 -0600

    added notes on next steps for trello

--- trelloCard.py
+++ trelloCard.py
@@ -7,11 +7,15 @@ def createCard(title):
     with open('keys.json') as json_file:
         data = json.load(json_file)
         apiKey = data['key']
-        token = data['secret']
+        #secret = data['secret']
+        token = data['token']
 
     url = "https://api.trello.com/1/cards"
 
+    # Need to get IdList 
+    # IdList is part of a board, so I need to get board ID then List ID then create a card on the listId
 
+    
     querystring = {"idList":"idList","keepFromSource":"all","key":apiKey,"token": token}
 
     print(querystring)
