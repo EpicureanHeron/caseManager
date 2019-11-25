facba40afd6032c88c975dd1c20e3a5a96da966b
epicureanheron <joethor1989@gmail.com>
2019-11-25 11:13:50 -0600

    creating trello card works, did not have a name or anything, however

--- trelloCard.py
+++ trelloCard.py
@@ -9,14 +9,18 @@ def createCard(title):
         apiKey = data['key']
         #secret = data['secret']
         token = data['token']
+        board = data['boardID']
 
     url = "https://api.trello.com/1/cards"
 
     # Need to get IdList 
     # IdList is part of a board, so I need to get board ID then List ID then create a card on the listId
 
+    # can get ID list by following instructions here: https://www.reddit.com/r/trello/comments/4axfcd/where_is_my_trello_board_id/ and parsing json
+
     
-    querystring = {"idList":"idList","keepFromSource":"all","key":apiKey,"token": token}
+    querystring = {"idList":board,"keepFromSource":"all","key":apiKey,"token": token}
+
 
     print(querystring)
 
